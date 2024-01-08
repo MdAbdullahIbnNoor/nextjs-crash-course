bafa40646a6613b5fbdd3610e213ecfaa34ce577
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2024-01-08 20:51:22 +0600

    Material UI installed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='p-8 text-center'>
+      <body className={inter.className}>
+        <nav>Home | About</nav>
+        <hr></hr>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <main>
-      <div>Home Page</div>
+    <main className="my-6">
+      <div className="border-2 mx-96 bg-sky-400">Home Page</div>
     </main>
   )
 }
